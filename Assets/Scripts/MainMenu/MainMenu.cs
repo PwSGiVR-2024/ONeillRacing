af0fded55ad09b22cbcf18454dbad7ceb4f78bfe
Karol Gorczany <kgorczany@gmail.com>
2024-05-29 11:12:02 +0200

    Main Menu
    -Grafiki
    -Czcionki
    -Opcje dźwięku
    -Opcje Menu Głównego

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,18 +2,35 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject MenuOptions;
+    [SerializeField] GameObject AudioOptions;
     public delegate void CameraDelegate(int cameraID);
     public CameraDelegate cameraDelegate;
 
-    public void CameraMain() {
+    // Main Menu
+    public void NewGame() {
         cameraDelegate(0);
     }
 
-    public void CameraLeft() {
+    public void Leadreboard() {
         cameraDelegate(1);
     }
 
-    public void CameraRight() {
+    public void Options() {
         cameraDelegate(2);
+        MenuOptions.SetActive(false);
+        AudioOptions.SetActive(true);
+    }
+
+    public void ExitGame() {
+
+    }
+
+    // Options Menu
+    public void BackToMain() {
+        cameraDelegate(0);
+        MenuOptions.SetActive(true);
+        AudioOptions.SetActive(false);
+
     }
 }
