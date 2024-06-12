0ba1f64d698a290ee33705e3da00e9a122b93371
PM-Kamil <118473847+PM-Kamil@users.noreply.github.com>
2024-06-12 18:42:52 +0200

    utrata świadomości, post processing
    - Dodano pakiet PostProcessing do projektu
    - usunięto niepotrzebny skrypt TestForceApplicator z prefaba GravityCar
    - GForceDisplay teraz dodatkowo kontroluje efekt vignette i colorGrading aby symulować utratę świadomości kierowcy spowodowaną wysoką siłą g
    - dodany efekt bloom do kamery w GravityCar

--- Assets/Scripts/GUI/GForceDisplay/GForceDisplay.cs
+++ Assets/Scripts/GUI/GForceDisplay/GForceDisplay.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Rendering.PostProcessing;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class GForceDisplay : MonoBehaviour
 {
@@ -10,16 +13,33 @@ public class GForceDisplay : MonoBehaviour
     float timeSinceContact = 100;
     float maxTimeSinceContact = 1f;
     float gForce = 0;
-    [SerializeField]
-    float minGForce = 0.29f;
 
     [SerializeField]
     TMP_Text textMesh;
+
+    [SerializeField]
+    float gLocThreshold = 4;//g-force at which g-LOC starts to happen
+
+    [SerializeField]
+    float gLocInnertia = 10;//how long g-LOC takes to set in and clear up
+
+    float accumulatedGLoc = 0;
+
+    [SerializeField]
+    GameObject carCamera;
+
+    [SerializeField]
+    PostProcessProfile pPProfile;
+    Vignette vignette;
+    ColorGrading colorGrading;
+
 
     private void Start()
     {
         textMesh.text = "0g";
         carGravity = GetComponent<Gravity>();
+        pPProfile.TryGetSettings<Vignette>(out vignette);
+        pPProfile.TryGetSettings<ColorGrading>(out colorGrading);
     }
 
     // Update is called once per frame
@@ -31,8 +51,28 @@ public class GForceDisplay : MonoBehaviour
         } else {
             gForce = carGravity.GetCentrifugalAcceleration().magnitude / 9.807f;
             textMesh.text = (Mathf.Round(100 * (1 - (timeSinceContact / maxTimeSinceContact)) * gForce ) / 100).ToString() + "g";
-            timeSinceContact += Time.fixedDeltaTime;
+            timeSinceContact += Time.deltaTime;
         }
+
+        if (gForce > gLocThreshold && accumulatedGLoc < 1)
+        {
+            accumulatedGLoc += (Time.deltaTime * (gForce/gLocThreshold))/gLocInnertia;
+
+        }
+
+        if (accumulatedGLoc > 0)
+        {
+            accumulatedGLoc -= Time.deltaTime * Mathf.Max(3*accumulatedGLoc,1) / gLocInnertia;
+        }
+
+        if (accumulatedGLoc < 0) { 
+            accumulatedGLoc = 0;
+        }
+
+        vignette.intensity.Override(accumulatedGLoc);
+        colorGrading.saturation.Override(-accumulatedGLoc*100);
+
+        print(accumulatedGLoc);
     }
 
     private void OnTriggerStay(Collider other)
